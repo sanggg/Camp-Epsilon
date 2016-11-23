dcd7a32a62ebde88a5e20842792cceff09ee5696
brnpham5 <bpham5@horizon.csueastbay.edu>
2016-11-22 18:52:14 -0800

    Updated State Machine
    Updated to work with DataFile_Handler

---
+++ GameState.py
@@ -0,0 +1,98 @@
+from StateMachine import *
+from DataFile_Handler import *
+
+##===================================================================
+## GameState
+
+Char = type ("Char", (object,), {})
+
+class GameState(Char):
+    def __init__(self):
+        self.StateMachine = StateMachine(self, self)
+        self.DataFile = DataFile_Handler("ACT1.txt")
+        self.Keyword = ""
+        self.Line = ""
+
+        ## States
+        self.StateMachine.addState("StartState", StartState(self.StateMachine))
+        self.StateMachine.addState("TransitionState", TransitionState(self.StateMachine))
+        self.StateMachine.addState("ReadState", ReadState(self.StateMachine))
+        self.StateMachine.addState("WaitState", WaitState(self.StateMachine))
+
+        ## Transitions
+        self.StateMachine.addTransition("toStartState", StateTransition("StartState"))
+        self.StateMachine.addTransition("toTransitionState", StateTransition("TransitionState"))
+        self.StateMachine.addTransition("toReadState", StateTransition("ReadState"))
+        self.StateMachine.addTransition("toWaitState", StateTransition("WaitState"))
+
+        ## Set first state to start state
+        self.StateMachine.setState("StartState")
+
+    def execute(self):
+        self.StateMachine.execute()
+
+    def callGUI_Manager(self):
+        #do stuff
+        pass
+
+    def Display_Start_Menu(self):
+        #self.GUI_Manager.Display_Start_Menu()
+        pass
+
+    def callDataFile_Handler(self):
+        self.Line = self.DataFile.keyword_Handler(-1)
+        #if(isinstance(self.Line, list)):
+        #    self.Keyword = self.Line[0]
+        #print(type(self.Line))
+        print (self.Line)
+
+    def DataFile_DSC_Handler(self):
+        self.DataFile.updateLine()
+        #self.GUI_Manager.printDSC(self.Line[1])
+
+    def DataFile_NPC_Handler(self):
+        self.DataFile.updateLine()
+        #self.GUI_Manager.printNPC(self.Line[1])
+
+    def DataFile_CHC_Handler(self):
+        #self.GUI_Manager.addChoices(self.Line[1], self.Line[2], self.Line[4], self.Line[5])
+        pass
+
+    def DataFile_SFX_Handler(self):
+        pass
+
+    def DataFile_MUS_Handler(self):
+        pass
+
+    def DataFile_BKG_Handler(self):
+        pass
+
+    def DataFile_LIK_Handler(self):
+        pass
+        
+    def DataFile_JMP_Handler(self):
+        self.DataFile.jumpToLine(self.Line[1])
+
+    def DataFile_FIN_Handler():
+        pass
+
+    
+
+    #def UserFile_FIN_Handler(self):  #player name, data file, likeability
+    #    DataFile.endAct(self.line[1])
+    #    act = DataFile.GetAct()
+    #    UserFile.updateUser([UserFile.getName(), DataFile.getAct(), UserFile.getLikeabilty()])
+    #    UserFile.SaveFile()
+
+    
+
+
+if __name__ == '__main__':
+    game = GameState()
+    for i in range(0, 20):
+        startTime = clock()
+        timeInterval = 1
+        while(startTime + timeInterval > clock()):
+            pass
+        game.execute()
+    
---
+++ StateMachine.py
@@ -0,0 +1,139 @@
+from time import clock
+from GameState import *
+
+##===================================================================
+## Transitions
+
+class StateTransition(object):
+    def __init__(self, toState):
+        self.toState = toState
+
+    def execute(self):
+        pass
+
+##===================================================================
+## States
+
+##Base class for states
+class State(object):
+    def __init__(self, StateMachine):
+        self.StateMachine = StateMachine
+
+    def enter(self):
+        pass
+
+    def execute(self):
+        pass
+        
+    def exit(self):
+        pass
+
+class StartState(State):
+    def __init__(self, StateMachine):
+        super(StartState, self).__init__(StateMachine)
+
+    def enter(self):
+        ##Call GameState.GUI_Manager to display the Start Menu
+        pass
+
+    def execute(self):
+        ##Wait until the user makes a playthrough choice
+        ##On New Game, call GameState.UserFile_Manager to create a new save file, then set transition to TransitionState
+        ##On Load Game, call GameState.UserFile_Manager to load existing save, then set transition to TransitionState
+        self.StateMachine.toTransition("toTransitionState")
+        pass
+        
+    def exit(self):
+        ##Call GameState.GUI_Manager to close the start menu, open the game menu
+        pass
+
+class TransitionState(State):
+    def __init__(self, StateMachine):
+        super(TransitionState, self).__init__(StateMachine)
+
+    def enter(self):
+        pass
+
+    def execute(self):
+        ##Call GameState.DataFile_Handler to open the sent DataFile name, set transition to ReadState
+        self.StateMachine.toTransition("toReadState")
+        pass
+        
+    def exit(self):
+        pass
+
+class ReadState(State):
+    def __init__(self, StateMachine):
+        super(ReadState, self).__init__(StateMachine)
+
+    def enter(self):
+
+        pass
+
+    def execute(self):
+        ##Call GameState.DataFile_Handler to parse keyword
+        ##Depending on the returned value, set transition to:
+            ##On ENC, WaitState
+            ##On FIN, TransitionState
+            ##On Anything else, ReadState
+        self.StateMachine.GameState.callDataFile_Handler()
+        self.StateMachine.toTransition("toReadState")
+        pass
+        
+    def exit(self):
+        pass
+
+class WaitState(State):
+    def __init__(self, StateMachine):
+        super(WaitState, self).__init__(StateMachine)
+
+    def enter(self):
+        pass
+
+    def execute(self):
+        ##Wait for user choice, print user choice, clear buttons, set line number to appropriate position
+        self.StateMachine.toTransition("toReadState")
+        pass
+        
+    def exit(self):
+        pass
+
+
+##===================================================================
+## Finite State Machine
+
+class StateMachine(object):
+    def __init__(self, character, GameState):
+        
+        ## Initialize state data
+        self.char = character
+        self.states = {}
+        self.transitions = {}
+        self.curState = None
+        self.prevState = None
+        self.trans = None
+
+        ##Initialize Reference to Game State
+        self.GameState = GameState
+
+    def addTransition(self, transName, transition):
+        self.transitions[transName] = transition
+
+    def addState(self, stateName, state):
+        self.states[stateName] = state
+
+    def setState(self, stateName):
+        self.prevState = self.curState
+        self.curState = self.states[stateName]
+
+    def toTransition(self, toTrans):
+        self.trans = self.transitions[toTrans]
+
+    def execute(self):
+        if(self.trans):
+            self.curState.exit()
+            self.trans.execute()
+            self.setState(self.trans.toState)
+            self.curState.enter()
+            self.trans = None
+        self.curState.execute()
